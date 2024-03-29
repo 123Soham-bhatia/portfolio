1f49c592933903a191ad0f670187f05572babc17
123Soham-bhatia <bhatiasoham013@gmail.com>
2024-03-30 00:23:02 +0530

    added href links to the projects

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -19,10 +19,10 @@ const ProjectCard = () => {
     return (
         <div className={`project-card ${animate ? 'animate' : ''}`}>
             <ul style={{ display:'flex',justifyContent:'space-between',padding:'3vh' }}>
-                <li><img style={{ width: '20vw', borderRadius: '20px', height: '23vh',cursor:'pointer' }} src={spotify} alt='spotify' /></li>
-                <li><img style={{ width: '20vw', borderRadius: '20px', height: '23vh',cursor:'pointer' }} src={news} alt='news' /></li>
-                <li><img style={{ width: '20vw', borderRadius: '20px', height: '23vh',cursor:'pointer' }} src={movie} alt='movie' /></li>
-                <li><div style={{ width: '20vw', borderRadius: '20px', height: '23vh', background: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '25px', fontFamily: 'monospace', fontWeight: 'bolder' }}>Working on more</div></li>
+                <li>  <a href='https://github.com/123Soham-bhatia' alt="github link" target='main' >  <img style={{ width: '20vw', borderRadius: '20px', height: '23vh',cursor:'pointer' }} src={spotify} alt='spotify' /></a> </li>
+                <li>  <a href='https://github.com/123Soham-bhatia/News-App.git'  alt="github link" target='main' >   <img style={{ width: '20vw', borderRadius: '20px', height: '23vh',cursor:'pointer' }} src={news} alt='news' /> </a>   </li>
+                <li> <a href='https://github.com/123Soham-bhatia' alt="github link" target='main' >   <img style={{ width: '20vw', borderRadius: '20px', height: '23vh',cursor:'pointer' }} src={movie} alt='movie' /></a></li>
+                <li>  <a  href='https://github.com/123Soham-bhatia' alt="github link" target='main'  >   <div style={{ width: '20vw', borderRadius: '20px', height: '23vh', background: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '25px', fontFamily: 'monospace', fontWeight: 'bolder' }}>Working on more</div></a></li>
             </ul>
         </div>
     );
